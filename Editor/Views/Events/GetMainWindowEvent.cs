3e73f5764977e89b1648897156a78d58d6937524
DrSmugleaf <DrSmugleaf@users.noreply.github.com>
2021-06-06 21:36:09 +0200

    Remove editor code

--- Editor/Views/Events/GetMainWindowEvent.cs
+++
@@ -1,9 +0,0 @@
-﻿using Avalonia.Interactivity;
-
-namespace Editor.Views.Events
-{
-    public class GetMainWindowEvent : RoutedEventArgs
-    {
-        public MainWindow MainWindow { get; set; } = default!;
-    }
-}
