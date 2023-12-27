9a6e11d499579dc581924e7050a245085e3c2123
DrSmugleaf <drsmugleaf@gmail.com>
2023-12-27 03:29:23 -0800

    Add method to get cardinal directions

---
+++ SpaceWizards.RsiLib/Directions/DirectionExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Immutable;
+
+namespace SpaceWizards.RsiLib.Directions;
+
+public static class DirectionExtensions
+{
+    private static readonly ImmutableArray<Direction> Cardinals =
+        ImmutableArray.Create(Direction.South, Direction.North, Direction.East, Direction.West);
+
+    public static ImmutableArray<Direction> GetCardinals()
+    {
+        return Cardinals;
+    }
+}
